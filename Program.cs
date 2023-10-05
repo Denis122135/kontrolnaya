c0be38fd926bd56defdde372c94136e0f37de3a2
Denis <www.00.www38@gmail.com>
2023-10-05 14:03:07 +0600

    Создание массива

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Этап 1: Создание массива 
+
+void FillArray(string [] FirstArray)
+{
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+        Console.WriteLine($"Введите {i + 1}элемент массива");
+        FirstArray[i] = Convert.ToString(Console.ReadLine()!); 
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов массива");
+int quantity = int.Parse(Console.ReadLine()!);
+string [] FirstArray = new string[quantity];
+FillArray(FirstArray);
