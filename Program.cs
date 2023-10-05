8da44ab500eb3322c3f503883639a70b62294ce0
Denis <www.00.www38@gmail.com>
2023-10-05 14:15:41 +0600

    этап 2

--- Program.cs
+++ Program.cs
@@ -4,13 +4,29 @@ void FillArray(string [] FirstArray)
 {
     for (int i = 0; i < FirstArray.Length; i++)
     {
-        Console.WriteLine($"Введите {i + 1}элемент массива");
+        Console.WriteLine($"Введите {i + 1} элемент массива");
         FirstArray[i] = Convert.ToString(Console.ReadLine()!); 
     }
 }
 
+// Этап 2: Печать массива
+
+void PrintArray(string [] FirstArray)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < FirstArray.Length; i ++)
+    {
+        Console.Write($"{FirstArray[i] + " "}");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+
+// Основное тело программы
 Console.Clear();
 Console.WriteLine("Введите количество элементов массива");
 int quantity = int.Parse(Console.ReadLine()!);
 string [] FirstArray = new string[quantity];
 FillArray(FirstArray);
+PrintArray(FirstArray);
